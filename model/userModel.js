ef49a2e03cd37a671aa61cbe4f1870eee272612c
helaman-Y-B <90625373+helaman-Y-B@users.noreply.github.com>
2025-09-19 23:51:17 +0000

    removed console.logs

--- model/userModel.js
+++ model/userModel.js
@@ -6,11 +6,9 @@ async function getData(user_id) {
 
     if (user_id !== undefined) {
         const userData = await db.collection('users').find({ _id: user_id }).toArray();
-        console.log(`user_img !== undefined executed: ${user_id}`);
         return userData;
     } else {
         const usersData = await db.collection('users').find({}).toArray();
-        console.log(`Else statement executed: ${user_id}`);
         return usersData;
     }
 }
